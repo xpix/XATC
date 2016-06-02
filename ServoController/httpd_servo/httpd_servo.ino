ed5598b250df9e55ceb8923037cca3fcbb120fd1
Frank Herrmann <xpixer@googlemail.com>
2016-06-02 09:09:37 +0000

    add long term test macro

--- ServoController/httpd_servo/httpd_servo.ino
+++ ServoController/httpd_servo/httpd_servo.ino
@@ -7,12 +7,16 @@ char* pass = ".........";
 
 /* --------------------------------------- */
 
-int position_zero = 5;
+int position_zero = 90;
 int position  = position_zero;
 int target    = 0;
 int pin       = 5; // Servo pin
-int wait      = 200;
+int wait      = 750;
 int readpin   = A0;
+int repeats   = 3;
+int realtarget= 0;
+bool failed   = false;
+
 
 
 ESP8266WebServer server(80);
@@ -37,22 +41,34 @@ void setup(void){
   Serial.println(WiFi.localIP());
   
   server.on("/", [](){
-    server.send(200, "text/plain", "Open /servo?value=90 to control servo or set the target value via /target?value=512");
+    server.send(200, "text/plain", "Open /servo?position=120&target=500&wait=500 to control servo or set the target value via /target?value=512");
   });
 
-  server.on("/target", [](){
-    String sval = server.arg("value");
-    target = sval.toInt();
-    server.send(200, "text/plain", info(position, target));
-  });
-
+  // http://ip/servo?position=120&target=500&wait=500
   server.on("/servo", [](){
-    String sval = server.arg("value");
+    String sval;
+
+    sval = server.arg("position");
     position = sval.toInt();
-    while(setServo(position) == 0){
-      Serial.println('Failed block, repeat ...');
+
+    if(sval = server.arg("target")){
+      target = sval.toInt();
     }
-    server.send(200, "text/plain", info(position, target));
+
+    if(sval = server.arg("wait")){
+      wait = sval.toInt();
+    }
+
+    int i = 1;
+    while(setServo(position) == 0 && i++ <= repeats){
+      delay(200);
+      Serial.println("Failed block, repeat ...");
+    }
+    if(failed){
+      server.send(500, "text/plain", info(position, target, realtarget));
+    } else {
+      server.send(200, "text/plain", info(position, target, realtarget));
+    }
   });
 
   server.begin();
@@ -66,8 +82,8 @@ void loop(void){
   server.handleClient();
 }
 
-String info(int position, int target){
-  String text = String("Position: " + String(position, 'DEC') + "\tTarget: " + String(target, 'DEC') + "\n");
+String info(int pos, int tar, int rtar){
+  String text = String("Position: " + String(pos, DEC) + "\tTarget: " + String(tar, DEC) + "\tRealTarget: " + String(rtar, DEC) + "\n");
   Serial.print(text);
   return text;
 }
@@ -79,12 +95,23 @@ int setServo(int pos){
   delay(wait);
 
   // check value on Analog input
-  int realtarget = analogRead(A0);
-  if(target > 0 && realtarget < target){
+  int rt = average(A0);
+  realtarget = rt;
+  if(target > 0 && rt < target && rt > (target-50){
+    failed = true; // not ok
     myservo.write(position_zero);
     delay(wait);
     return 0;
   }
+  failed = false; // ok
   return realtarget;
 } 
  
+int average(int port){
+  int all = 0;
+  for (int i = 0; i < 10; i++) {
+    all += analogRead(port);
+    delay(20);
+  }
+  return (all / 10);
+}
